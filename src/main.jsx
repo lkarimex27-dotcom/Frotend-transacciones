222f7e243f5f17c9fa9a86b60165ca1d692ee413
lkarimex27-dotcom <lkarimex27@gmail.com>
2026-04-08 18:15:12 -0500

    subiendo main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { AuthProvider } from "./features/auth/context/AuthContext.jsx";
+import { AuthProvider } from "./features/auth/context/AuthContext";
 //import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
