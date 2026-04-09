47ca0edd8a0c5c29e75c0f2f42d9747aa9c4c842
lkarimex27-dotcom <lkarimex27@gmail.com>
2026-04-08 21:04:31 -0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppRoutes } from './AppRoutes'
-import { AuthProvider } from './features/auth/context/AuthContext'
+import { AuthProvider } from "./features/auth/context/AuthContext.jsx";
 
 export default function App() {
   return (
