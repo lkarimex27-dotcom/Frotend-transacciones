83b4da73825c26530de884c8d78aba90d51593f4
lkarimex27-dotcom <lkarimex27@gmail.com>
2026-04-08 21:51:46 -0500

    prueba2

--- src/features/auth/api/axios.js
+++ src/features/auth/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+baseURL: import.meta.env.VITE_API_URL || 'https://backend-transacciones.onrender.com/api/auth',
 headers: { 'Content-Type': 'application/json' },
 });
 // Interceptor para adjuntar el token si existe:
