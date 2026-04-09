74d6b38c38204fb7f7bcb185b4e53d58a0716453
lkarimex27-dotcom <lkarimex27@gmail.com>
2026-04-08 21:24:34 -0500

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppRoutes } from './AppRoutes'
-import { AuthProvider } from "../AuthContext.jsx";
+import { AuthProvider } from "./features/auth/context/AuthContext.jsx";
 
 export default function App() {
   return (
--- src/features/auth/components/transactionList.jsx
+++ src/features/auth/components/transactionList.jsx
@@ -1,4 +1,4 @@
-import TransactionItem from './TransactionItem';
+import TransactionItem from './transactionItem';
 
 export default function TransactionList({ items, onChange }) {
     if (!items || !items.length) {
