831e969cec9353b3919fa8345c572f97376c9538
luxis17 <lkarimex27@gmail.com>
2026-04-08 21:06:51 -0500

    Fix import path for AuthProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppRoutes } from './AppRoutes'
-import { AuthProvider } from "./features/auth/context/AuthContext.jsx";
+import { AuthProvider } from "./features/auth/context/Authcontext.jsx";
 
 export default function App() {
   return (
